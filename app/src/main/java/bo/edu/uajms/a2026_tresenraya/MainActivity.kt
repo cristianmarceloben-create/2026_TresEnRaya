16202cc17025d4a2cf9cf0048d93eb629f08ce7b
Cristian Marcelo Benitez <cristianbenitez@gmail.com>
2026-08-26 10:18:27 -0400

    P07 Diseño Activity Tres en Raya

---
+++ app/src/main/java/bo/edu/uajms/a2026_tresenraya/MainActivity.kt
@@ -0,0 +1,12 @@
+package bo.edu.uajms.a2026_tresenraya
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
